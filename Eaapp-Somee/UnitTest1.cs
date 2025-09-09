a5deafe2531af4e99fe07848970de476c1c4fe3f
adragu <andredragu1699@gmail.com>
2025-09-09 12:01:28 +0300

    Add initial files

---
+++ Eaapp-Somee/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Eaapp_Somee
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
